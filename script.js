62f529781cfa868530eb8891acc4e4078db98a97
qbetadev <157713329+qbetadev@users.noreply.github.com>
2024-07-17 17:18:27 +0300

    Add function for game with 5 rounds

--- script.js
+++ script.js
@@ -37,38 +37,68 @@ function showSelectionComputer(selectionComputer) {
 }
 
 //Single round function
-function playRound(scoreComputer, scoreHuman) {
+function playRound() {
     let roundWinner = "";
     let randomNumber = getRandomIntInclusive(1, 3);
     let selectionHuman = getHumanChoice();
     let selectionComputer = getComputerChoice(randomNumber);
     if (selectionHuman === selectionComputer) {
+        roundWinner = "tie";
         showSelectionHuman(selectionHuman);
         showSelectionComputer(selectionComputer);
         console.log("It's a tie!");
-        console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
-        roundWinner = "tie";
     } else if (
         (selectionHuman === "rock" && selectionComputer === "paper") ||
         (selectionHuman === "paper" && selectionComputer === "scissors") ||
         (selectionHuman === "scissors" && selectionComputer === "rock")
     ) {
+        roundWinner = "computer";
         showSelectionHuman(selectionHuman);
         showSelectionComputer(selectionComputer);
         console.log(`You lose, ${selectionComputer} beats ${selectionHuman}!`);
-        scoreComputer += 1;
-        console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
-        roundWinner = "computer";
     } else if (
         (selectionHuman === "rock" && selectionComputer === "scissors") ||
         (selectionHuman === "paper" && selectionComputer === "rock") ||
         (selectionHuman === "scissors" && selectionComputer === "paper")
     ) {
+        roundWinner = "human";
         showSelectionHuman(selectionHuman);
         showSelectionComputer(selectionComputer);
         console.log(`You won, ${selectionHuman} beats ${selectionComputer}!`);
-        scoreHuman += 1;
-        console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
-        roundWinner = "human";
+    }
+    return roundWinner;
+}
+
+//5 rounds game
+function playGame() {
+    let scoreComputer = 0;
+    let scoreHuman = 0;
+    let roundWinner;
+
+    for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
+        roundWinner = playRound();
+        switch(roundWinner) {
+            case "tie":
+                console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
+                break;
+            case "computer":
+                scoreComputer += 1;
+                console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
+                break;
+            case "human":
+                scoreHuman += 1;
+                console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
+                break;
+        }
+    }
+
+    if (scoreHuman > scoreComputer) {
+        console.log("You won the game!");
+    } else if (scoreHuman< scoreComputer) {
+        console.log("You lose the game!");
+    } else {
+        console.log("It's a tie!");
     }
 }
+
+playGame();
