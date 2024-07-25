8f5327d3963ce92beb2f682dfee24a9e1039c874
qbetadev <157713329+qbetadev@users.noreply.github.com>
2024-07-25 13:47:32 +0300

    Add UI and single round logic

--- script.js
+++ script.js
@@ -22,83 +22,86 @@ function getComputerChoice(randomNumber) {
   return choiceComputer;
 }
 
-//Asking user to input his choice
-function getHumanChoice() {
-    let choiceHuman = prompt("Choose rock, paper or scissors").toLowerCase();
-    return choiceHuman;
-}
-
 function showSelectionHuman(selectionHuman) {
-    console.log("Your choice - " + selectionHuman);
+    return("Your choice - " + selectionHuman);
 }
 
 function showSelectionComputer(selectionComputer) {
-    console.log("Computer choice - " + selectionComputer);
+    return("Computer choice - " + selectionComputer)
 }
 
 //Single round function
-function playRound() {
+function playRound(selectionHuman) {
     let roundWinner = "";
     let randomNumber = getRandomIntInclusive(1, 3);
-    let selectionHuman = getHumanChoice();
     let selectionComputer = getComputerChoice(randomNumber);
     if (selectionHuman === selectionComputer) {
         roundWinner = "tie";
-        showSelectionHuman(selectionHuman);
-        showSelectionComputer(selectionComputer);
-        console.log("It's a tie!");
+        humanChoiceText.textContent = showSelectionHuman(selectionHuman);
+        computerChoiceText.textContent = showSelectionComputer(selectionComputer);
+        roundWinnerText.textContent = "It's a tie!";
     } else if (
         (selectionHuman === "rock" && selectionComputer === "paper") ||
         (selectionHuman === "paper" && selectionComputer === "scissors") ||
         (selectionHuman === "scissors" && selectionComputer === "rock")
     ) {
         roundWinner = "computer";
-        showSelectionHuman(selectionHuman);
-        showSelectionComputer(selectionComputer);
-        console.log(`You lose, ${selectionComputer} beats ${selectionHuman}!`);
+        humanChoiceText.textContent = showSelectionHuman(selectionHuman);
+        computerChoiceText.textContent = showSelectionComputer(selectionComputer);
+        roundWinnerText.textContent = `You lose, ${selectionComputer} beats ${selectionHuman}!`
     } else if (
         (selectionHuman === "rock" && selectionComputer === "scissors") ||
         (selectionHuman === "paper" && selectionComputer === "rock") ||
         (selectionHuman === "scissors" && selectionComputer === "paper")
     ) {
         roundWinner = "human";
-        showSelectionHuman(selectionHuman);
-        showSelectionComputer(selectionComputer);
-        console.log(`You won, ${selectionHuman} beats ${selectionComputer}!`);
+        humanChoiceText.textContent = showSelectionHuman(selectionHuman);
+        computerChoiceText.textContent = showSelectionComputer(selectionComputer);
+        roundWinnerText.textContent = `You won, ${selectionHuman} beats ${selectionComputer}!`
     }
     return roundWinner;
 }
 
 //5 rounds game
-function playGame() {
-    let scoreComputer = 0;
-    let scoreHuman = 0;
-    let roundWinner;
+// function playGame() {
+//     let scoreComputer = 0;
+//     let scoreHuman = 0;
+//     let roundWinner;
 
-    for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
-        roundWinner = playRound();
-        switch(roundWinner) {
-            case "tie":
-                console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
-                break;
-            case "computer":
-                scoreComputer += 1;
-                console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
-                break;
-            case "human":
-                scoreHuman += 1;
-                console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
-                break;
-        }
-    }
+//     for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
+//         roundWinner = playRound();
+//         switch(roundWinner) {
+//             case "tie":
+//                 console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
+//                 break;
+//             case "computer":
+//                 scoreComputer += 1;
+//                 console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
+//                 break;
+//             case "human":
+//                 scoreHuman += 1;
+//                 console.log(`Computer score - ${scoreComputer}, your score ${scoreHuman}`);
+//                 break;
+//         }
+//     }
 
-    if (scoreHuman > scoreComputer) {
-        console.log("You won the game!");
-    } else if (scoreHuman< scoreComputer) {
-        console.log("You lose the game!");
-    } else {
-        console.log("It's a tie!");
-    }
-}
+//     if (scoreHuman > scoreComputer) {
+//         console.log("You won the game!");
+//     } else if (scoreHuman< scoreComputer) {
+//         console.log("You lose the game!");
+//     } else {
+//         console.log("It's a tie!");
+//     }
+// }
 
-playGame();
+const buttonChoice = document.querySelectorAll('.button');
+const humanChoiceText = document.querySelector('.humanChoice');
+const computerChoiceText = document.querySelector('.computerChoice');
+const roundWinnerText = document.querySelector('.winner')
+
+buttonChoice.forEach((button) => {
+    button.addEventListener('click', () => {
+        let selectionHuman = button.textContent.toLowerCase();
+        playRound(selectionHuman);
+    });
+});
